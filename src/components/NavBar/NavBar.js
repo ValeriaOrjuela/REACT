d94868b52367c4e715098f8bb0ed6eb088ce9bc6
“Cindy <cindyvorjuela@outlook.com>
2024-12-25 13:27:23 -0500

    entrega1

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return(
+        <nav className="NavBarr">
+            <h3>JuvaLicious</h3>
+            <div className = "sep"> 
+                <button>
+                    Inicio
+                </button>
+                <button>
+                    Productos
+                </button>
+                <button>
+                    Sale
+                </button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
